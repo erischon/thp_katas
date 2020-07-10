258d181eaf02c16e1cc98d59a1ad85e42bc8dd99
erischon <erischon@gmail.com>
2020-07-10 21:00:08 +0200

    Katas

---
+++ kata-7-Disemvowel_Trolls.rb
@@ -0,0 +1,7 @@
+def disemvowel(str)
+  str.delete "aeiouAEIOU"
+end
+
+
+puts disemvowel("This website is for losers LOL!")
+# "Ths wbst s fr lsrs LL!")
---
+++ kata-7-Square_Every_Digit.rb
@@ -0,0 +1,9 @@
+def square_digits num
+  # code goes here
+end
+
+puts square_digits(3212)
+puts square_digits(2112)
+puts square_digits(1111)
+puts square_digits(1234321)
+puts square_digits(0)
