21f84bb87e83c4f795a9a84024befac7ca3c516c
Eri Schon <erischon@gmail.com>
2020-07-11 16:43:21 +0200

    On va y arriver...

--- kata_4.rb
+++
@@ -1,8 +0,0 @@
-class String
-  def toJadenCase
-    return (self.split(" ").map { |word| word.capitalize }).join(" ")
-  end
-end
-
-str = "How can mirrors be real if our eyes aren't real"
-puts str.toJadenCase()
